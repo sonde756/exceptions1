f208a919cdae13748303526b25fb114f12865eb8
sonde756 <edsonrafael756@gmail.com>
2023-03-26 21:44:27 -0300

    melhor solucao

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.SimpleTimeZone;
@@ -12,7 +14,10 @@ public class Reservation {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut){
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Erro in reservation: Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -40,20 +45,18 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut){
 
         Date now = new Date();
-        if (checkIn.before(now) || checkOut.before(now)){
-           return "Reservatin dates for update must be future dates";
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservatin dates for update must be future dates");
         }
         if (!checkOut.after(checkIn)) {
-            return "Erro in reservation: Check-out date must be after check-in date";
+            throw new DomainException("Erro in reservation: Check-out date must be after check-in date");
         }
-
         this.checkIn = checkIn;
         this.checkOut = checkOut;
 
-        return null;
     }
 
     @Override
@@ -62,6 +65,6 @@ public class Reservation {
                 ", checkIn " + sdf.format(checkIn) +
                 ", checkOut " + sdf.format(checkOut)
                 + ", " + duration() + " nights";
-        
+
     }
 }
---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class DomainException extends RuntimeException{
+
+
+    public DomainException(String msg){
+        super(msg);
+    }
+}
